f8cd1163e9841d30e04c58ba3c6a6d571e770810
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2023-10-18 21:47:33 -0500

    Update ProtobootappApplication.java

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -21,7 +21,7 @@ public class ProtobootappApplication {
     //Se ejecuta con http://localhost:8000/
     @GetMapping("/")
     public String index() {
-        return String.format("Bienvenido! Esta aplicación ayuda a elevar números a sus potencias y a entender AWS.");
+        return String.format("Bienvenido! Esta aplicación ayuda a elevar números a sus potencias y a entender AWS mucho mejor.");
     }    
 
     //Se ejecuta con http://localhost:8000/hola o http://localhost:8000/hola?nombre=John
